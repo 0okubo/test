ae5dcf7cfec9f7a114999047199a71ef4a5f2dd6
0okubo <ontama3390@gmail.com>
2018-11-10 13:46:50 +0900

    test commit

---
+++ script/script.js
@@ -0,0 +1,109 @@
+﻿/* photoCategoryButton 1 Sake */
+
+function CB1C() { // 1_アイコン画像に切り替え
+  document.getElementById('CBa1').innerHTML = '<img src=\"./image/Sake.png\" class=\"CategoryButtonLogo\">';
+  document.getElementById('CBa1').style.width = '78px'; //solidで囲う分マイナス2pxして調整
+  document.getElementById('CBa1').style.height = '18px'; //solidで囲う分マイナス2pxして調整
+  document.getElementById('CBa1').style.backgroundColor = '#FFFFFF';
+  document.getElementById('CBa1').style.borderStyle = 'solid';
+  document.getElementById('CBa1').style.borderWidth = '1px';
+  document.getElementById('CBa1').style.borderColor = '#4c4c4c';
+  document.getElementById('topPhoto').innerHTML = '<img src=\"./image/Sake.jpg\" class=\"categoryPhoto\">';
+};
+
+function CB1R() { // 1_文字に戻し
+  document.getElementById('CBa1').innerHTML = 'Sake!';
+  document.getElementById('CBa1').style.width = '80px';
+  document.getElementById('CBa1').style.height = '20px';
+  document.getElementById('CBa1').style.backgroundColor = '#4c4c4c'
+  document.getElementById('CBa1').style.borderStyle = '';
+  document.getElementById('topPhoto').innerHTML = '<div id=\"topPhoto\">Photo!</div>';
+};
+
+/* photoCategoryButton 2 Ramen */
+
+function CB2C() { // 2_アイコン画像に切り替え
+  document.getElementById('CBa2').innerHTML = '<img src=\"./image/Ramen.png\" class=\"CategoryButtonLogo\">';
+  document.getElementById('CBa2').style.width = '78px'; //solidで囲う分マイナス2pxして調整
+  document.getElementById('CBa2').style.height = '18px'; //solidで囲う分マイナス2pxして調整
+  document.getElementById('CBa2').style.backgroundColor = '#FFFFFF';
+  document.getElementById('CBa2').style.borderStyle = 'solid';
+  document.getElementById('CBa2').style.borderWidth = '1px';
+  document.getElementById('CBa2').style.borderColor = '#4c4c4c';
+  document.getElementById('topPhoto').innerHTML = '<img src=\"./image/Ramen.jpg\" class=\"categoryPhoto\">';
+};
+
+function CB2R() { // 2_文字に戻し
+  document.getElementById('CBa2').innerHTML = 'Ramen!';
+  document.getElementById('CBa2').style.width = '80px';
+  document.getElementById('CBa2').style.height = '20px';
+  document.getElementById('CBa2').style.backgroundColor = '#4c4c4c'
+  document.getElementById('CBa2').style.borderStyle = '';
+  document.getElementById('topPhoto').innerHTML = '<div id=\"topPhoto\">Photo!</div>';
+};
+
+/* photoCategoryButton 3 Ryouri */
+
+function CB3C() { // 3_アイコン画像に切り替え
+  document.getElementById('CBa3').innerHTML = '<img src=\"./image/Ryouri.png\" class=\"CategoryButtonLogo\">';
+  document.getElementById('CBa3').style.width = '78px'; //solidで囲う分マイナス2pxして調整
+  document.getElementById('CBa3').style.height = '18px'; //solidで囲う分マイナス2pxして調整
+  document.getElementById('CBa3').style.backgroundColor = '#FFFFFF';
+  document.getElementById('CBa3').style.borderStyle = 'solid';
+  document.getElementById('CBa3').style.borderWidth = '1px';
+  document.getElementById('CBa3').style.borderColor = '#4c4c4c';
+  document.getElementById('topPhoto').innerHTML = '<img src=\"./image/Ryouri.jpg\" class=\"categoryPhoto\">';
+};
+
+function CB3R() { // 3_文字に戻し
+  document.getElementById('CBa3').innerHTML = 'Ryouri!';
+  document.getElementById('CBa3').style.width = '80px';
+  document.getElementById('CBa3').style.height = '20px';
+  document.getElementById('CBa3').style.backgroundColor = '#4c4c4c'
+  document.getElementById('CBa3').style.borderStyle = '';
+  document.getElementById('topPhoto').innerHTML = '<div id=\"topPhoto\">Photo!</div>';
+};
+
+/* photoCategoryButton 4 Ryokou */
+
+function CB4C() { // 4_アイコン画像に切り替え
+  document.getElementById('CBa4').innerHTML = '<img src=\"./image/Ryokou.png\" class=\"CategoryButtonLogo\">';
+  document.getElementById('CBa4').style.width = '78px'; //solidで囲う分マイナス2pxして調整
+  document.getElementById('CBa4').style.height = '18px'; //solidで囲う分マイナス2pxして調整
+  document.getElementById('CBa4').style.backgroundColor = '#FFFFFF';
+  document.getElementById('CBa4').style.borderStyle = 'solid';
+  document.getElementById('CBa4').style.borderWidth = '1px';
+  document.getElementById('CBa4').style.borderColor = '#4c4c4c';
+  document.getElementById('topPhoto').innerHTML = '<img src=\"./image/Ryokou.jpg\" class=\"categoryPhoto\">';
+};
+
+function CB4R() { // 4_文字に戻し
+  document.getElementById('CBa4').innerHTML = 'Ryokou!';
+  document.getElementById('CBa4').style.width = '80px';
+  document.getElementById('CBa4').style.height = '20px';
+  document.getElementById('CBa4').style.backgroundColor = '#4c4c4c'
+  document.getElementById('CBa4').style.borderStyle = '';
+  document.getElementById('topPhoto').innerHTML = '<div id=\"topPhoto\">Photo!</div>';
+};
+
+/* photoCategoryButton 5 Sonota */
+
+function CB5C() { // 5_アイコン画像に切り替え
+  document.getElementById('CBa5').innerHTML = '<img src=\"./image/Sonota.png\" class=\"CategoryButtonLogo\">';
+  document.getElementById('CBa5').style.width = '78px'; //solidで囲う分マイナス2pxして調整
+  document.getElementById('CBa5').style.height = '18px'; //solidで囲う分マイナス2pxして調整
+  document.getElementById('CBa5').style.backgroundColor = '#FFFFFF';
+  document.getElementById('CBa5').style.borderStyle = 'solid';
+  document.getElementById('CBa5').style.borderWidth = '1px';
+  document.getElementById('CBa5').style.borderColor = '#4c4c4c';
+  document.getElementById('topPhoto').innerHTML = '<img src=\"./image/Sonota.jpg\" class=\"categoryPhoto\">';
+};
+
+function CB5R() { // 5_文字に戻し
+  document.getElementById('CBa5').innerHTML = 'Sonota!';
+  document.getElementById('CBa1').style.width = '80px';
+  document.getElementById('CBa1').style.height = '20px';
+  document.getElementById('CBa5').style.backgroundColor = '#4c4c4c'
+  document.getElementById('CBa5').style.borderStyle = '';
+  document.getElementById('topPhoto').innerHTML = '<div id=\"topPhoto\">Photo!</div>';
+};
